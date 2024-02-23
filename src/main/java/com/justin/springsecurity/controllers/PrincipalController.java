349df5ecd936ab8d3ef34ab07e8a51f66c0cb537
Agus9217 <justinibarrola92@gmail.com>
2024-02-23 16:47:10 -0300

    create controller

--- src/main/java/com/justin/springsecurity/controllers/PrincipalController.java
+++ src/main/java/com/justin/springsecurity/controllers/PrincipalController.java
@@ -1,9 +1,17 @@
 package com.justin.springsecurity.controllers;
 
+import com.justin.springsecurity.model.ERole;
+import com.justin.springsecurity.model.RoleEntity;
+import com.justin.springsecurity.model.UserEntity;
+import com.justin.springsecurity.model.request.CreateUserDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 public class PrincipalController {
@@ -18,10 +26,10 @@ public class PrincipalController {
         return "Hello World Secured";
     }
 
-    /*@PostMapping("/create")
-    public ResponseEntity<?>createUser(){
+    @PostMapping("/createUser")
+    public ResponseEntity<?>createUser(@Valid @RequestBody CreateUserDTO createUserDTO){
 
-    }*/
+    }
 
 
 }
---
+++ src/main/java/com/justin/springsecurity/model/request/CreateUserDTO.java
@@ -0,0 +1,60 @@
+package com.justin.springsecurity.model.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Set;
+
+public class CreateUserDTO {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String username;
+    private String password;
+    private Set<String>roles;
+
+    public CreateUserDTO() {
+    }
+
+    public CreateUserDTO(String email, String username, String password, Set<String> roles) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
